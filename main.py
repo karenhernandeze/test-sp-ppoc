b2951523b249c5d4072d905728713489b39844ab
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 21:59:22 -0600

    fixe

--- main.py
+++ main.py
@@ -7,17 +7,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import os
 from fastapi_sqlalchemy import DBSessionMiddleware
-# from fastapi_sqlalchemy import db
+from fastapi_sqlalchemy import db
 from models import Notes as ModelUser
 from schema import Notes as SchemaUser
 from dotenv import load_dotenv
 
-# DATABASE_URL = "postgresql://postgres:12345678@database-2.ckgekixw3q0p.us-east-1.rds.amazonaws.com/postgres"
-# database = databases.Database(DATABASE_URL)
-# # metadata = sqlalchemy.MetaData()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
-
 
 app = FastAPI()
 
@@ -27,3 +23,11 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 @app.get("/")
 def hello():
     return {"message":"Hello TutLinks.com"}
+
+@app.get("/notes/", response_model=SchemaUser)
+def create_user():
+    # db_user = ModelUser(
+    #     text=user.first_name, last_name=user.last_name, age=user.age
+    # )
+    note = db.session.query(ModelUser).first()
+    return note
