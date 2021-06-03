b06295b8bac84bd22e35b4762c92eed69c6069d4
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-03 03:47:58 -0600

    changes

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
 # main.py
+import uvicorn
 from fastapi import FastAPI
-from typing import List
-# import databases
-# import sqlalchemy
-from fastapi import FastAPI
-from pydantic import BaseModel
 import os
-import psycopg2
 from fastapi_sqlalchemy import DBSessionMiddleware
 from fastapi_sqlalchemy import db
 from models import Notes as ModelUser
 from schema import Notes as SchemaUser
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# load_dotenv(os.path.join(BASE_DIR, ".env"))
+import os
+import psycopg2
+import dj_database_url
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+# DATABASE_URL = os.environ['HEROKU_POSTGRESQL_YELLOW_URL']
+
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASE_URL = os.environ['DATABASE_URL'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
+
 
 app = FastAPI()
 
-# app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
-db_url=os.environ.get('HEROKU_POSTGRESQL_YELLOW_URL')
-conn = psycopg2.connect(db_url, sslmode='require')
+# app.add_middleware(DBSessionMiddleware, db_url=os.environ["HEROKU_POSTGRESQL_YELLOW_URL"])
+# db_url=os.environ.get('HEROKU_POSTGRESQL_YELLOW_URL')
+# conn = psycopg2.connect(db_url, sslmode='require')
 
 
 @app.get("/")
 def hello():
     return {"message":"Hello TutLinks.com"}
 
-# @app.get("/notes/", response_model=List[SchemaUser])
-# def create_user():
+# @app.get("/notes/", response_model=SchemaUser)
+# def get_user():
 #     # db_user = ModelUser(
 #     #     text=user.first_name, last_name=user.last_name, age=user.age
 #     # )
-#     note = db.session.query(ModelUser)
+#     print(db.session.query(ModelUser).count())
+#     note = db.session.query(ModelUser).get(8)
 #     return note
-
-@app.get("/notes/", response_model=SchemaUser)
-def get_user():
-    # db_user = ModelUser(
-    #     text=user.first_name, last_name=user.last_name, age=user.age
-    # )
-    print(db.session.query(ModelUser).count())
-    note = db.session.query(ModelUser).get(8)
-    return note
 
 
 @app.post("/user/", response_model=SchemaUser)
@@ -53,3 +54,7 @@ def create_user(user: SchemaUser):
     db.session.add(db_user)
     db.session.commit()
     return db_user
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
