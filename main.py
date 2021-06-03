421da6e58923ccf5cd33dee7676f5c540ef6805f
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 21:36:48 -0600

    fixe

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # database = databases.Database(DATABASE_URL)
 # # metadata = sqlalchemy.MetaData()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 app = FastAPI()
