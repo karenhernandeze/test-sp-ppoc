6fbeea76940c1173a03c4bc8b883400f88c63039
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 21:17:27 -0600

    fixes

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ from dotenv import load_dotenv
 
 # DATABASE_URL = "postgresql://postgres:12345678@database-2.ckgekixw3q0p.us-east-1.rds.amazonaws.com/postgres"
 # database = databases.Database(DATABASE_URL)
-# metadata = sqlalchemy.MetaData()
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+# # metadata = sqlalchemy.MetaData()
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 app = FastAPI()
 
-app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+# app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 @app.get("/")
 def hello():
