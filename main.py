724bfa1371c7923256e652b48187eaf0c8bb51a5
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 20:43:38 -0600

    added database url

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # main.py
 from fastapi import FastAPI
+from typing import List
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+DATABASE_URL = "postgresql://postgres:12345678@database-2.ckgekixw3q0p.us-east-1.rds.amazonaws.com/postgres"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
 app = FastAPI()
 
 @app.get("/")
