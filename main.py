10e40d226d32982f7961242a97311e491a082b2c
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 21:12:22 -0600

    fix

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ from typing import List
 # import sqlalchemy
 from fastapi import FastAPI
 from pydantic import BaseModel
+import os
+from fastapi_sqlalchemy import DBSessionMiddleware
+from fastapi_sqlalchemy import db
+# from models import User as ModelUser
+# from schema import User as SchemaUser
+from dotenv import load_dotenv
 
 # DATABASE_URL = "postgresql://postgres:12345678@database-2.ckgekixw3q0p.us-east-1.rds.amazonaws.com/postgres"
 # database = databases.Database(DATABASE_URL)
 # metadata = sqlalchemy.MetaData()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 @app.get("/")
 def hello():
